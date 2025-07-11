27dbce42d8ec4b81b583a38dcf7363ac6420fa42
Akash Biswas <akashbiswassa1@gmail.com>
2025-07-11 06:01:44 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function redirectToLink() {
+  window.location.href = "https://www.profitableratecpm.com/gtv7jzmca?key=a921e730cccec67c533d71bb6a062093";
+}
+
+// Redirect automatically after 10 seconds
+setTimeout(redirectToLink, 10000);
